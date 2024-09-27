9eb6ee6fdba42bef8d52b6d896df936379e9c4fc
Liam <2022214880@stu.cqupt.edu.cn>
2024-09-27 21:47:29 +0800

    fix: reset edge detection in jk_ff_tb.sv

--- lab2/jk_ff/jk_ff.sv
+++ lab2/jk_ff/jk_ff.sv
@@ -1,20 +1,27 @@
-module jk_ff(
-    input j,
-    input k,
-    input clk,
-    input reset,
-    output reg q
-    );
-    always @(posedge clk or posedge reset)
-    begin
-        if(reset)
+module jk_ff (
+    input  logic j,
+    input  logic k,
+    input  logic clk,
+    input  logic reset, // 使用低电平有效复位
+    output logic q
+);
+    always_ff @(negedge clk or negedge reset) begin
+        if (!reset) begin
             q <= 1'b0;
-        else
-        begin
-            if(j & ~k)
+        end
+        else begin
+            if (j & ~k) begin
                 q <= 1'b1;
-            else if(~j & k)
+            end
+            else if (~j & k) begin
                 q <= 1'b0;
+            end
+            else if (j & k) begin
+                q <= ~q;
+            end
+            else begin
+                q <= q; // 保持当前状态
+            end
         end
     end
 endmodule
