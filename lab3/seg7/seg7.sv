a084e5fed16eb109c2b59cbc63548e6e1f423697
Liam <2022214880@stu.cqupt.edu.cn>
2024-10-07 23:33:37 +0800

    fix the capitalization of the a,c,e

--- lab3/seg7/seg7.sv
+++ lab3/seg7/seg7.sv
@@ -22,11 +22,11 @@ always_comb begin
             4'h7: seg_out = 7'b1111000; // '7'
             4'h8: seg_out = 7'b0000000; // '8'
             4'h9: seg_out = 7'b0010000; // '9'
-            4'hA: seg_out = 7'b0100000; // 'A'
+            4'hA: seg_out = 7'b0001000; // 'A'
             4'hB: seg_out = 7'b0000011; // 'B'
-            4'hC: seg_out = 7'b0100111; // 'C'
+            4'hC: seg_out = 7'b1000110; // 'C'
             4'hD: seg_out = 7'b0100001; // 'D'
-            4'hE: seg_out = 7'b0000100; // 'E'
+            4'hE: seg_out = 7'b0000110; // 'E'
             4'hF: seg_out = 7'b0001110; // 'F'
         endcase
 end
