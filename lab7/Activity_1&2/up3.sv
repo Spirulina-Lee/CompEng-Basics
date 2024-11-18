778795e097b4c1288e28b015a3f0b934177cdd6c
Liam <2022214880@stu.cqupt.edu.cn>
2024-11-18 23:50:45 +0800

    重构 up3 模块，暴露内部信号为输出，更新测试基准，完善测试逻辑

--- lab7/Activity_1&2/up3.sv
+++ lab7/Activity_1&2/up3.sv
@@ -1,16 +1,14 @@
 module up3 (
     input logic [9:0] SW,              // SW[0]: STORE_MEM, SW[1]: FETCH, SW[2]: INCR_PC, SW[3]: LOAD_PC, SW[4]: LOAD_IRL, SW[5]: LOAD_IRU, SW[6]: LOAD_AC
     input logic [1:0] KEY,             // KEY[0]: clk, KEY[1]: reset
-    output logic [6:0] HEX0, HEX1,     // HEX0、HEX1 显示 addr/value
-    output logic [6:0] HEX2, HEX3,     // HEX2、HEX3 显示 opcode
-    output logic [6:0] HEX4, HEX5,     // HEX4、HEX5 显示 PC
-    output logic [9:0] LEDR            // 指示状态
+    output logic [9:0] LEDR,           // 指示状态
+    // 将内部信号暴露为输出，以供 tb 使用
+    output logic [7:0] ir_lower,       // IRL 的输出 (addr/value)
+    output logic [7:0] ir_upper,       // IRU 的输出 (opcode)
+    output logic [7:0] pc_out          // PC 的输出
 );
 
     // 内部信号声明
-    logic [7:0] pc_out;                // PC 的输出
-    logic [7:0] ir_upper;              // IRU 的输出 (opcode)
-    logic [7:0] ir_lower;              // IRL 的输出 (addr/value)
     logic [7:0] alu_result;            // ALU 的输出 (Z)
     logic [7:0] ram_out;               // RAM 的输出 (MDR)
     logic [7:0] address;               // RAM 地址
@@ -85,33 +83,6 @@ module up3 (
         .data(ac_out),
         .wren(STORE_MEM),
         .q(ram_out)
-    );
-
-    // 显示 addr/value 到 HEX0 和 HEX1
-    dual_seg7 display_addr_value (
-        .bin_in(ir_lower),
-        .blank(1'b0),
-        .test(1'b0),
-        .out1(HEX1),
-        .out0(HEX0)
-    );
-
-    // 显示 opcode 到 HEX2 和 HEX3
-    dual_seg7 display_opcode (
-        .bin_in(ir_upper),
-        .blank(1'b0),
-        .test(1'b0),
-        .out1(HEX3),
-        .out0(HEX2)
-    );
-
-    // 显示 PC 到 HEX4 和 HEX5
-    dual_seg7 display_pc (
-        .bin_in(pc_out),
-        .blank(1'b0),
-        .test(1'b0),
-        .out1(HEX5),
-        .out0(HEX4)
     );
 
     // 指示状态到 LEDR
